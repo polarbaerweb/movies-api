05ed5a604a404169e8a10a484e2144c120713fd1
Polar Bear <xadjimurodaliyev@gmail.com>
2023-04-19 21:36:56 +0500

    Add tempaltes/

--- Movies/urls.py
+++ Movies/urls.py
@@ -2,10 +2,17 @@ from django.conf.urls.static import static
 from django.conf import settings as confSettings
 from MovieList import settings
 from django.urls import path
-from .views import MoviesInfo, MovieDetail, api_root
+from .views import MoviesInfo, MovieDetail, api_root, PersonViewSet
+from rest_framework.routers import DefaultRouter
+from django.urls import include
+
+router = DefaultRouter()
+router.register(r"people", PersonViewSet, basename="person")
+
 
 urlpatterns = [
-    path('', api_root, name="main"),
+    # path('', api_root, name="main"),
     path('movies-list/', MoviesInfo.as_view(), name="movies-list"),
-    path('moviesList/movie/<int:pk>/', MovieDetail.as_view(), name="movie-detail")
+    path('moviesList/movie/<int:pk>/', MovieDetail.as_view(), name="movie-detail"),
+    path('', include(router.urls))
 ] + static(confSettings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Movies/views.py
+++ Movies/views.py
@@ -1,10 +1,12 @@
 from rest_framework import generics
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from . import seralizer
-from .models import movies_data
+from .models import movies_data, Person
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import renderers
 
 
 @api_view(["GET"])
@@ -30,3 +32,14 @@ class MovieDetail(generics.RetrieveUpdateAPIView):
     permission_classes = (
         IsAuthenticatedOrReadOnly,
     )
+
+
+class PersonViewSet(ModelViewSet):
+    queryset = Person.objects.all()
+    serializer_class = seralizer.PersonCurrent
+
+    @action(detail=True, renderer_classes=[renderers.TemplateHTMLRenderer], url_name="byid", url_path="byid")
+    def byid(self, request, pk=None):
+        person = self.get_object()
+        response = Response(template_name="index.html", data={"data": person})
+        return response
